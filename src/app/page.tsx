d55131664862e6a59d388332ffec039b4baaa091
a-s-t-e-y-a <krishnoit100@gmail.com>
2025-03-13 17:32:49 +0530

    FEAT : setup for the tailwindcss is fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <div className="text-4xl font-bold">
+          sampel text
+        </div>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
